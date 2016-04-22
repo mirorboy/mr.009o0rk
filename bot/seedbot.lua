9cee77974219a6b6332fa0f86a1973faa0e455d4
mirorboy <aliarvan0901@gmail.com>
2016-04-23 01:08:00 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {215122858
+    sudo_users = {215122858}
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
